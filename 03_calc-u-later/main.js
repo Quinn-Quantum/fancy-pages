b8d70ac31791fd5b79b97e3a41e4a5f2db1bfe7f
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2025-04-04 20:57:17 +0200

    03: Calculator can now perform simple calculations and the code has been modernized

--- 03_calc-u-later/main.js
+++ 03_calc-u-later/main.js
@@ -1,23 +1,68 @@
-var calcNums = document.querySelectorAll('.numbtn div');
-for (var i = 0; i < calcNums.length; i++) {
-    calcNums[i].addEventListener("click", function(p) { pushToOutput(p.currentTarget.innerText); });
-}
+const  calcNums = document.querySelectorAll('.numbtn div');
+calcNums.forEach(btn => {
+    btn.addEventListener("click", () => pushToOutput(btn.innerText));
+});
 
-var calcOps = document.querySelectorAll('.opsbtns div');
-for (var i = 0; i < calcOps.length; i++) {
-    calcOps[i].addEventListener("click", function(p) { pushToOutput(p.currentTarget.title); });
-}
+const  calcOps = document.querySelectorAll('.opsbtns div');
+calcOps.forEach(btn => {
+    btn.addEventListener("click", () => pushToOutput(btn.title));
+});
 
-document.getElementById('equals').addEventListener("click", function() { calculate(); });
-document.getElementById('reset').addEventListener("click", function() { reset(); });
+document.getElementById('equals').addEventListener("click", calculate);
+document.getElementById('reset').addEventListener("click",  reset);
 
 function pushToOutput(value) {
     document.getElementById("output").innerText += value;
 }
 
 function calculate() {
-    var output = document.getElementById("output");
-    output.innerText = "Not Implemented";
+    const output = document.getElementById("output");
+    const outputText = output.innerText.trim()
+
+    let result;
+
+    if(outputText.includes("+")){
+        const [a,b] =outputText.split("+").map(Number)
+        result  = add(a,b) 
+    }
+    else if(outputText.includes("-")){
+        const [a,b] =outputText.split("-").map(Number)
+        result  = subtract(a,b)  
+    }
+    else if(outputText.includes("*")){
+        const [a,b]=outputText.split("*").map(Number)
+        result  = multiblikation(a,b) 
+    }
+    else if(outputText.includes("/")){
+        const [a,b]=outputText.split("/").map(Number)
+        result  = substration(a,b)
+    }else{
+        result = "Invalid expression";
+    }
+    // output.innerText = "Not Implemented";
+    output.innerText = result
+}
+
+function add(num1,num2){
+    return num1 + num2
+
+}
+function subtract(num1,num2){
+    return num1 - num2
+    
+}
+function multiblikation(num1,num2){
+    return num1 * num2
+}
+function substration(num1,num2){
+    if(num2 === 0){
+        return "Division by 0 not possible";
+    }
+    else{
+        return num1 / num2
+        
+    }
+    
 }
 
 function reset() {
